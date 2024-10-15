69103ef13a6ad1a658193321cb662a0e9f4576da
frederik-uni <147479464+frederik-uni@users.noreply.github.com>
2024-10-15 14:57:44 +0200

    add comment

--- projects/up.sql
+++ projects/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS projects(
     p_id serial PRIMARY KEY NOT NULL,
     name text NOT NULL,
-    team_ids integer[] NOT NULL,
+    team_ids integer[] NOT NULL, -- dupliate data that is not needed but reduces processing time
     start_date date NOT NULL,
     end_date date,
     description text,
