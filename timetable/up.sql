72baf76a083144d365f20606a808183e74e5b579
frederik-uni <147479464+frederik-uni@users.noreply.github.com>
2024-10-15 14:49:19 +0200

    update timetable structure

--- timetable/up.sql
+++ timetable/up.sql
@@ -1,6 +1,9 @@
 CREATE TABLE IF NOT EXISTS timetable(
     t_id serial PRIMARY KEY NOT NULL,
-    project_id integer NOT NULL,
-    user_id integer NOT NULL,
+    -- project_id integer NOT NULL,
+    -- user_id integer NOT NULL,
+    c_id: integer NOT NULL,
+    start_time timestamptz NOT NULL,
+    end_time timestamptz NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now(),
 );
