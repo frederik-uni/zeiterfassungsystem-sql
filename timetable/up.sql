831419032a891077831e79bc55af330f53528fd8
frederik-uni <147479464+frederik-uni@users.noreply.github.com>
2024-10-15 14:56:35 +0200

    .

--- timetable/up.sql
+++ timetable/up.sql
@@ -5,5 +5,7 @@ CREATE TABLE IF NOT EXISTS timetable(
     c_id: integer NOT NULL,
     start_time timestamptz NOT NULL,
     end_time timestamptz NOT NULL,
+    description text NOT NULL,
+    created_by integer NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now(),
 );
