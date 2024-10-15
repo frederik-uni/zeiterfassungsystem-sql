00592d0b0b653162c012d6e61e3611abf1014efc
frederik-uni <147479464+frederik-uni@users.noreply.github.com>
2024-10-15 14:51:39 +0200

    add roles to other tables

--- costtable/up.sql
+++ costtable/up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS costtable(
     c_id serial PRIMARY KEY NOT NULL,
     u_id integer NOT NULL,
     p_id integer NOT NULL,
+    t_id integer NOT NULL,
     cost numeric NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now(),
 );
